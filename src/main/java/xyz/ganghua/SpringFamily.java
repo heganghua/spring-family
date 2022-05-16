d561aba0e2b8de4726f49b7c0a65dcdea67e1703
heganghua <heganghua1113@163.com>
2022-05-16 16:49:16 +0800

    init

---
+++ src/main/java/xyz/ganghua/SpringFamily.java
@@ -0,0 +1,12 @@
+package xyz.ganghua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFamily {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFamily.class, args);
+    }
+}
